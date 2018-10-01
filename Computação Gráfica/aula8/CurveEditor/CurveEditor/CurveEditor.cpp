fdbbd88f3d596da613c1883d600dc682eb1424d3
Rafael Nossal <rafael.nossal@gmail.com>
2018-10-01 00:40:51 -0300

    Continuidade do trabalho de curvas de CG

--- Computação Gráfica/aula8/CurveEditor/CurveEditor/CurveEditor.cpp
+++ Computação Gráfica/aula8/CurveEditor/CurveEditor/CurveEditor.cpp
@@ -12,26 +12,6 @@
 //==========================================================================================================
 int dotQuantity = 4;
 Dot dots[4];
-void drawDotConnectionLine() {
-	int i;
-
-	glColor3f(getFloatColor(21), getFloatColor(101), getFloatColor(192));
-	glLineWidth(2.f);
-
-	glBegin(GL_LINE_STRIP);
-	for (i = 0; i < dotQuantity; i++) {
-		glVertex2d(dots[i].point.x, dots[i].point.y);
-	}
-	glEnd();
-
-	glFlush();
-}
-void drawDots() {
-	int i;
-	for (i = 0; i < dotQuantity; i++) {
-		drawDot(dots[i].point.x, dots[i].point.y);
-	}
-}
 
 void drawBezier() {
 	float t;
@@ -41,8 +21,8 @@ void drawBezier() {
 	for (t = 0.f; t <= 1.f; t += 0.0002) {
 
 		glVertex2d(
-			calcCubicBezier(t, dots[0].point.x, dots[1].point.x, dots[2].point.x, dots[3].point.x),
-			calcCubicBezier(t, dots[0].point.y, dots[1].point.y, dots[2].point.y, dots[3].point.y)
+			calcCubicBezier(t, dots[0].location.x, dots[1].location.x, dots[2].location.x, dots[3].location.x),
+			calcCubicBezier(t, dots[0].location.y, dots[1].location.y, dots[2].location.y, dots[3].location.y)
 		);
 	}
 	glEnd();
@@ -50,26 +30,22 @@ void drawBezier() {
 	glFlush();
 }
 
+
+
 void testThings() {
-	dots[0].point = { 200, 150 };
-	dots[1].point = { 250, 150 };
-	dots[2].point = { 250, 50 };
-	dots[3].point = { 300, 50 };
+	dots[0].location = { 200, 150 };
+	dots[1].location = { 250, 150 };
+	dots[2].location = { 250, 50 };
+	dots[3].location = { 300, 50 };
 
-	drawDotConnectionLine();
-	drawDots();
-	drawBezier();
-
-	dots[0].point = { 300, 50 };
-	dots[1].point = { 350, 50 };
-	dots[2].point = { 350, 150 };
-	dots[3].point = { 400, 150 };
-
-	drawDotConnectionLine();
-	drawDots();
 	drawBezier();
 }
 //==========================================================================================================
+
+// ========== Variáveis globais. ===========
+Curve *curves = NULL;
+int curvesLimit = INITIAL_ALOC;
+int currentCurve = 0;
 
 // ========== Handlers da janela. ==========
 /**
@@ -88,6 +64,10 @@ void draw() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
 
+	drawDotConnectionLines();
+	drawDots();
+	drawDotConnectionBeziers();
+
 	testThings();
 }
 /**
@@ -95,7 +75,11 @@ void draw() {
 */
 void mouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		drawDot(x, y);
+		curves[currentCurve].currentDot++;
+		curves[currentCurve].dots[curves[currentCurve].currentDot].location.x = x;
+		curves[currentCurve].dots[curves[currentCurve].currentDot].location.y = y;
+
+		draw();
 	}
 }
 /**
@@ -113,11 +97,45 @@ void reshape(int w, int h) {
 
 // ========== Funções de apoio. ==========
 /**
+	Inicia as estruturas e variáveis básicas para o funcionamento.
+*/
+void initStructures() {
+	int i;
+
+	curves = (Curve *)malloc(INITIAL_ALOC * sizeof(Curve));
+
+	for (i = 0; i < INITIAL_ALOC; i++) {
+		curves[i].dots = NULL;
+		curves[i].dotsLimit = INITIAL_ALOC;
+		curves[i].currentDot = -1;
+	}
+
+	curves[0].dots = (Dot *)malloc(INITIAL_ALOC * sizeof(Dot));
+	for (i = 0; i < INITIAL_ALOC; i++) {
+		curves[0].dots[i].location.x = 0;
+		curves[0].dots[i].location.y = 0;
+	}
+
+	curves[0].dots[0].location.x = 250;
+	curves[0].dots[0].location.y = 150;
+
+	curves[0].dots[1].location.x = 300;
+	curves[0].dots[1].location.y = 150;
+
+	curves[0].dots[2].location.x = 300;
+	curves[0].dots[2].location.y = 50;
+
+	curves[0].dots[3].location.x = 350;
+	curves[0].dots[3].location.y = 50;
+
+	curves[0].currentDot = 3;
+}
+/**
 	Desenha um ponto vazado na tela.
 	@param x Posição x.
 	@param y Posição y.
 */
-void drawDot(int x, int y) {
+void drawDot(Dot *dot) {
 	int i = 0,
 		numberLines = DOT_RADIUS * 2;
 
@@ -126,17 +144,57 @@ void drawDot(int x, int y) {
 
 	glBegin(GL_TRIANGLE_FAN);
 	for (i = 0; i < numberLines; i++) {
-		glVertex2f(x + (DOT_MIDDLE_RADIUS * cos(i * 2.f * M_PI / numberLines)), y + (DOT_MIDDLE_RADIUS * sin(i * 2.f * M_PI / numberLines)));
+		glVertex2f(dot->location.x + (DOT_MIDDLE_RADIUS * cos(i * 2.f * M_PI / numberLines)), dot->location.y + (DOT_MIDDLE_RADIUS * sin(i * 2.f * M_PI / numberLines)));
 	}
 	glEnd();
 
 	glBegin(GL_LINE_LOOP);
 	for (i = 0; i < numberLines; i++) {
-		glVertex2f(x + (DOT_CIRCLE_RADIUS * cos(i * 2.f * M_PI / numberLines)), y + (DOT_CIRCLE_RADIUS * sin(i * 2.f * M_PI / numberLines)));
+		glVertex2f(dot->location.x + (DOT_CIRCLE_RADIUS * cos(i * 2.f * M_PI / numberLines)), dot->location.y + (DOT_CIRCLE_RADIUS * sin(i * 2.f * M_PI / numberLines)));
 	}
 	glEnd();
 
 	glFlush();
+}
+/**
+	Desenha todos os pontos que estão armazenados no programa.
+*/
+void drawDots() {
+	int i, j;
+	for (i = 0; i <= currentCurve; i++) {
+		for (j = 0; j <= curves[i].currentDot; j++) {
+			drawDot(&curves[i].dots[j]);
+		}
+	}
+}
+/**
+	Desenha as linhas de conexão dos pontos de uma curva.
+*/
+void drawDotConnectionLine(Curve *curve) {
+	int i;
+
+	glColor3f(getFloatColor(21), getFloatColor(101), getFloatColor(192));
+	glLineWidth(2.f);
+
+	glBegin(GL_LINE_STRIP);
+
+	for (i = 0; i <= curve->currentDot; i++) {
+		glVertex2d(curve->dots[i].location.x, curve->dots[i].location.y);
+	}
+
+	glEnd();
+
+	glFlush();
+}
+/**
+	Desenha todas as linhas de conexão de todas as curvas que estão armazenados no programa.
+*/
+void drawDotConnectionLines() {
+	int i;
+
+	for (i = 0; i <= currentCurve; i++) {
+		drawDotConnectionLine(&curves[i]);
+	}
 }
 /**
 	Converte o range de RGB decimal (0 a 255) em float para as funções do OpenGL que tem parâmetros de cor.
@@ -145,25 +203,11 @@ float getFloatColor(int value) {
 	return value / 255.f;
 }
 /**
-	Faz o cálculo da curva Bezier para quatro pontos.
-	@param t Valor de parametrização.
-	@param point0 Primeiro ponto.
-	@param point1 Segundo ponto.
-	@param point2 Terceiro ponto.
-	@param point3 Quarto ponto.
-*/
-float calcCubicBezier(float t, float point0, float point1, float point2, float point3) {
-	float tDiff = 1 - t;
-
-	return (pow(tDiff, 3) * point0) + (3 * t * pow(tDiff, 2) * point1) + (3 * pow(t, 2) * tDiff * point2) + (pow(t, 3) * point3);
-}
-/**
 	Faz o cálculo do coeficiente binomial que é usado no cálculo de curva Bezier.
 	@param n "Parte de cima" do cálculo.
 	@param k "Parte de baixo" do cálculo.
 */
-int calculeBinomialCoefficient(int n, int k)
-{
+int calculeBinomialCoefficient(int n, int k) {
 	int res = 1;
 
 	if (k > n - k) k = n - k;
@@ -176,6 +220,71 @@ int calculeBinomialCoefficient(int n, int k)
 
 	return res;
 }
+/**
+	Faz o cálculo de curva Bezier dos pontos usando o calculo de triângulo de Pascal para fugir de ser somente uma curva cúbica.
+	@param t Valor de parametrização.
+	@param curve Curva a serem pegos os pontos para ser calculado.
+*/
+Point calculePointBezier(float t, Curve *curve) {
+	Point point;
+	int i;
+	float tDiff = 1 - t;
+
+	point.x = 0;
+	point.y = 0;
+	for (i = 0; i <= curve->currentDot; i++) {
+		point.x += calculeBinomialCoefficient(curve->currentDot, i) * pow(tDiff, curve->currentDot - i) * pow(t, i) * curve->dots[i].location.x;
+		point.y += calculeBinomialCoefficient(curve->currentDot, i) * pow(tDiff, curve->currentDot - i) * pow(t, i) * curve->dots[i].location.y;
+	}
+
+	return point;
+}
+/**
+	Desenha a curva Bezier de uma Determinada curva.
+*/
+void drawBezier(Curve *curve) {
+	int i;
+	float t;
+	glColor3f(getFloatColor(244), getFloatColor(67), getFloatColor(54));
+
+	if (curve->currentDot > 1) {
+		glBegin(GL_POINTS);
+		for (t = 0.f; t <= 1.f; t += 0.0002) {
+			Point point = calculePointBezier(t, curve);
+
+			glVertex2d(
+				point.x,
+				point.y
+			);
+		}
+		glEnd();
+	}
+
+	glFlush();
+}
+/**
+	Desenha todas as curvar Bezier de todas as curvas que estão armazenados no programa.
+*/
+void drawDotConnectionBeziers() {
+	int i;
+
+	for (i = 0; i <= currentCurve; i++) {
+		drawBezier(&curves[i]);
+	}
+}
+/**
+	Faz o cálculo da curva Bezier para quatro pontos (Aqui só a nível de histórico da fórmula).
+	@param t Valor de parametrização.
+	@param point0 Primeiro ponto.
+	@param point1 Segundo ponto.
+	@param point2 Terceiro ponto.
+	@param point3 Quarto ponto.
+*/
+float calcCubicBezier(float t, float point0, float point1, float point2, float point3) {
+	float tDiff = 1 - t;
+
+	return (pow(tDiff, 3) * point0) + (3 * t * pow(tDiff, 2) * point1) + (3 * pow(t, 2) * tDiff * point2) + (pow(t, 3) * point3);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -187,6 +296,7 @@ int main(int argc, char *argv[]) {
 	glutDisplayFunc(draw);
 	glutMouseFunc(mouse);
 	glutReshapeFunc(reshape);
+	initStructures();
 	initScreen();
 	glutMainLoop();
 
--- Computação Gráfica/aula8/CurveEditor/CurveEditor/CurveEditor.h
+++ Computação Gráfica/aula8/CurveEditor/CurveEditor/CurveEditor.h
@@ -5,18 +5,27 @@
 #define DOT_CIRCLE_RADIUS (DOT_MIDDLE_RADIUS + 3)
 #define DOT_CIRCLE_WIDTH 2
 #define DOT_RADIUS (DOT_CIRCLE_RADIUS + (DOT_CIRCLE_WIDTH / 2))
+#define INITIAL_ALOC 20
 
 // ======================== Estruturas =========================
 typedef struct Point
 {
-	int x;
-	int y;
-};
+	float x;
+	float y;
+} Point;
 
 typedef struct Dot
 {
-	Point point;
-};
+	Point location;
+} Dot;
+
+typedef struct Curve
+{
+	Dot *dots;
+	int dotsLimit;
+	int currentDot;
+
+} Curve;
 
 // ==================== Handlers da janela. ====================
 void initScreen();
@@ -25,7 +34,14 @@ void mouse(int button, int state, int x, int y);
 void reshape(int w, int h);
 
 // ===================== Funções de apoio. =====================
-void drawDot(int x, int y);
+void initStructures();
+void drawDot(Dot *dot);
+void drawDots();
+void drawDotConnectionLine(Curve *curve);
+void drawDotConnectionLines();
 float getFloatColor(int value);
-float calcCubicBezier(float t, float point0, float point1, float point2, float point3);
 int calculeBinomialCoefficient(int n, int k);
+Point calculePointBezier(float t, Curve *curve);
+void drawBezier(Curve *curve);
+void drawDotConnectionBeziers();
+float calcCubicBezier(float t, float point0, float point1, float point2, float point3);
